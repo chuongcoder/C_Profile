c0d05b43ee7ec1961b81750066217f2b8de3d640
chuongdev <chuongminh3225@gmail.com>
2023-12-19 11:26:02 +0700

    new

---
+++ profile.js
@@ -0,0 +1,18 @@
+var Typed = new Typed (".multiple-text",{
+    strings: ["a Tiktoker","a ITER","a YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
+document.addEventListener("DOMContentLoaded", function() {
+    // Truy cập thẻ audio
+    var audio = document.getElementById("noelAudio");
+
+    // Bắt đầu chơi nhạc khi trang web tải xong
+    audio.play();
+});
+document.addEventListener("click", function() {
+    var audio = document.getElementById("noelAudio");
+    audio.play();
+  });
