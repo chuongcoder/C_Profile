5baee04d06e9249d6b91478fa5c1efcd33e7035c
chuongminh32 <23110187@student.hcmute.edu.vn>
2024-11-10 17:44:14 +0700

    edit ava

--- profile.js
+++ profile.js
@@ -1,18 +1,18 @@
-var Typed = new Typed (".multiple-text",{
-    strings: ["a Tiktoker","a ITER","a YouTuber"],
+var Typed = new Typed(".multiple-text", {
+    strings: ["a Coder", "a UTEr", "a Student"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
-    loop:true
-})
-document.addEventListener("DOMContentLoaded", function() {
+    loop: true,
+});
+document.addEventListener("DOMContentLoaded", function () {
     // Truy cập thẻ audio
     var audio = document.getElementById("noelAudio");
 
     // Bắt đầu chơi nhạc khi trang web tải xong
     audio.play();
 });
-document.addEventListener("click", function() {
+document.addEventListener("click", function () {
     var audio = document.getElementById("noelAudio");
     audio.play();
-  });
+});
